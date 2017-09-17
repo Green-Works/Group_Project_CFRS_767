9a689b683400a789be96236124b38682041a0945
Brian Daugette <31937700+briandaugette@users.noreply.github.com>
2017-09-17 18:06:54 -0400

    Update dictionarySplitter_v3.py
    Added some error checking to make sure the dictionary exists.
    Cleaned up and formatted output strings to user. Added text when program successfully exits.
    Added hard coded file path for dictionary list
    Changed names of split dictionary lists from "wordlist_x.txt" to "xofy.txt" where x is the number of a node and y is the number of total nodes (workers).

--- dictionarySplitter_v3.py
+++ dictionarySplitter_v3.py
@@ -10,17 +10,19 @@ TODO:
 
 """
 
+import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # Comment out the line below to enable logging to the terminal
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 nodes = 0
 passwordCounter = 0
-test = 'false'
+test = 'False'
+logging.info(type(test))
 
 # Ensure the user enters an integer greater than 0 for the number of nodes
-while test == 'false':
+while test == 'False':
     try:
         nodes = (int(input("Enter the number of AWS instances that will be used (cannot be 0): ")))
         logging.debug("nodes is: " + str(nodes))
@@ -30,16 +32,22 @@ while test == 'false':
         continue
     else:
         if nodes > 0:
-            test = 'true'
+            test = 'True'
 
 # Read through the password list and build an array of all the passwords
-myfile = open('test_dictionary.txt', 'r', encoding="utf-8")
-
-# Count the number of passwords
-for line in myfile:
-    passwordCounter += 1
-
-myfile.close()
+# Right now the destination to the dictionary is hard coded, but can be changed
+# to accept user input
+try:
+    myfile = open('J:\\CFRS 767\\GroupProject\\test_dictionary.txt', 'r', encoding="utf-8")
+    # Count the number of passwords
+    for line in myfile:
+        passwordCounter += 1
+    myfile.close()
+except FileNotFoundError:
+    msg = "Cannot find the dictionary. Exiting program."
+    print(msg)
+    logging.error(msg)
+    sys.exit(1)
 
 
 # Determine how many passwords will be passed to each node.
@@ -47,11 +55,12 @@ logging.debug("passwordCounter is: " + str(passwordCounter))
 countPerNode = int((passwordCounter / nodes))
 logging.debug("countPerNode is: " + str(countPerNode))
 
-myfile = open('test_dictionary.txt', 'r', encoding="utf-8")
+myfile = open('J:\\CFRS 767\\GroupProject\\test_dictionary.txt', 'r', encoding="utf-8")
 for i in range(nodes):
     # create a password sheet for each node
-    o = open("wordlist_" + str(i + 1) + ".txt", "w", encoding="utf-8")
-    logging.debug("Creating wordlist_" + str(i + 1) + ".txt")
+    filename = "{0}of{1}.txt".format(i + 1, nodes)
+    o = open(filename, "w", encoding="utf-8")
+    logging.debug("Creating wordlist " + filename)
 
     # for each new password list that is not the last password list, enter passwords
     for j in range(countPerNode):
@@ -60,18 +69,9 @@ for i in range(nodes):
 
 # for the last password list write all remaining passwords from the original list
 for line in myfile:
-    o = open("wordlist_" + str(nodes) + ".txt", "w", encoding="utf-8")
+    o = open("{}of{}.txt".format(nodes, nodes), "a", encoding="utf-8")
     o.write(str(line.strip()) + "\n")
 
 o.close()
 myfile.close()
-
-
-
-
-
-
-
-
-
-
+print("The program has completed. You should have {} newly created split password lists.".format(nodes))
