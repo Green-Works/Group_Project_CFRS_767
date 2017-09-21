bfdffe5d7c2a69a28b9c0bf21f9543a84bb03bd0
adrockthemc <2282668+adrockthemc@users.noreply.github.com>
2017-09-20 20:08:56 -0400

    Update job_manager_v2.py

--- job_manager_v2.py
+++ job_manager_v2.py
@@ -15,8 +15,11 @@
 #   MODULES REQUIRED:
 #       - argparse
 #       - http.server
-#		   - logging
-#   SCRIPT VER: 0.2
+#		- logging
+#       - netifaces
+#       - netaddr
+#       - socket
+#   SCRIPT VER: 0.3
 #   REQURIED FILES:
 #       - /etc/job_manager/job_manager.config
 #
@@ -34,6 +37,9 @@
 import argparse
 import http.server
 import logging
+import netifaces
+import netaddr
+import socket
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # Comment out the line below to enable logging to the terminal
 # logging.disable(logging.CRITICAL)
@@ -47,12 +53,55 @@ PARSE.add_argument("-t", "--type", help="the type of hash supplied. Supported op
 PARSE.add_argument("-p", "--port", type=int, help="TCP port workers are using")
 ARGS = PARSE.parse_args()
 
-# this module discovers the workers up on the wire
+# credit to https://gist.github.com/gkbrk/99442e1294a6c83368f5#file-scanner-py-L46 for this function
+# it scans a ip:port and returns whether or not the port is open.
+def is_port_open(host, port): #Return boolean
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        sock.connect((host, port))
+    except socket.error:
+        return False
+    return True
+'''
+This module discovers the workers up on the wire based on a specific open port.
+Further error checking could be done on this to make sure a worker was listening on the given port
+and not some other service (i.e. a specific GET request)
+'''
 def worker_discover(PORT):
-    print("This part of the script should discover the workers running on the network")
-    print("Searching for workers on tcp port %s" % PORT)
-    WORKER_LIST = ["worker1", "worker2", "worker3"]
+    WORKER_LIST = []
+    logging.debug("Searching for workers on local network using tcp port %s" % PORT)
+    for X in netifaces.interfaces():
+        INTERFACE_DATA = netifaces.ifaddresses(X).get(netifaces.AF_INET)
+        IP = INTERFACE_DATA[0]['addr']
+        NETMASK = INTERFACE_DATA[0]['netmask']
+        CIDR = netaddr.IPAddress(NETMASK).netmask_bits()
+        netaddr.IPAddress(IP)
+        NETWORK = IP + "/" + str(CIDR)
+        netaddr.IPNetwork(NETWORK)
+        logging.debug("Interface: %s \tIP: %s\tNETMASK: %s" % (X, IP, CIDR))
+        type(IP)
+        #Special check for the local host otherwise the scan would take forever since localhost CIDR is /8
+        if IP[0:3] == "127":
+            is_open = is_port_open(IP, PORT)
+            if is_open is True:
+                WORKER_LIST.extend([str(IP)])
+                logging.debug("port %s open on localhost" % (PORT))
+            else:
+                logging.debug("port %s is NOT open on localhost" % (PORT))
+        #this is the scan for all other interfaces. Variable 'Y' contains a single ip address in the CIDR range
+        else:
+            for Y in netaddr.IPNetwork(NETWORK):
+                if str(Y) == IP:
+                    logging.debug("skipping scan on IP: %s" % str(Y))
+                else:
+                    is_open = is_port_open(str(Y), PORT)
+                    if is_open is True:
+                        WORKER_LIST.extend([str(Y)])
+                        logging.debug("port %s open on IP %s" % (PORT, str(Y)))
+        logging.debug("Workers found: %s" % WORKER_LIST)
     return(WORKER_LIST)
+
 
 # this listens for responses from the workers and responds
 def worker_monitor(WORKER_LIST):
